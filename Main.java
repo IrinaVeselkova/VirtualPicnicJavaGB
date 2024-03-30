b7c2d6f46126b1d3fdd3dc3c91295285e1fa6cfb
Veselkova Irina <veselkovaiv@yandex.ru>
2024-03-30 18:19:27 +0300

    added class Methods

--- Main.java
+++ Main.java
@@ -9,64 +9,20 @@ public class Main {
         ArrayList<String> picnic = new ArrayList<>(List.of(scanner.nextLine().strip().split(" ")));
         System.out.println("Всего на пикник взяли " + picnic.size() + " продукта(ов)\n");
         System.out.println("Продукты с самыми длинными названиями:");
-        ArrayList<String> maxLengthWords = maxLengthWord(picnic);
+        ArrayList<String> maxLengthWords = Methods.maxLengthWord(picnic);
         for (String string : maxLengthWords) {
-            System.out.println(capitalized(string)
+            System.out.println(Methods.capitalized(string)
                     + " - "
                     + string.length()
                     + " cимволов");
         }
 
-        TreeMap<String, Integer> sort = new TreeMap<>(countProducts(picnic));
+        TreeMap<String, Integer> sort = new TreeMap<>(Methods.countProducts(picnic));
         System.out.println("\nВ корзине для пикника сейчас:");
-        sortedTreeMapByValue(sort, maxLengthWords.getFirst().length());
+        Methods.sortedTreeMapByValue(sort, maxLengthWords.getFirst().length());
 
     }
 
-    public static ArrayList<String> maxLengthWord(ArrayList<String> picnic) {
-        picnic.sort(Comparator.comparingInt(String::length));
-        ArrayList<String> maxLengthSizeWords = new ArrayList<>();
-        int maxSizeWord = picnic.getLast().length();
-        for (int i = picnic.size() - 1; i > 0; i--) {
-            if (picnic.get(i).length() == maxSizeWord && !maxLengthSizeWords.contains(picnic.get(i).toLowerCase())) {
-                maxLengthSizeWords.add(picnic.get(i).toLowerCase());
-            } else break;
-        }
-        return maxLengthSizeWords;
-    }
 
-    public static HashMap<String, Integer> countProducts(ArrayList<String> picnic) {
-        HashMap<String, Integer> countProduct = new HashMap<>();
-        for (String product : picnic) {
-            if (!countProduct.containsKey(product.toLowerCase())) {
-                countProduct.put(product.toLowerCase(), 1);
-            } else {
-                int count = (Integer) countProduct.get(product.toLowerCase()) + 1;
-                countProduct.put(product.toLowerCase(), count);
-            }
-        }
-        return countProduct;
-    }
-
-    public static String capitalized(String string) {
-        return string.substring(0, 1).toUpperCase() + string.substring(1);
-    }
-
-    public static void sortedTreeMapByValue(TreeMap<String, Integer> treeMap, int maxLengthWords) {
-        TreeSet<Integer> values = new TreeSet<>(Comparator.reverseOrder());
-        values.addAll(treeMap.values());
-        Set<String> keySet = treeMap.keySet();
-        for (Integer i : values) {
-            for (String key : keySet) {
-                if (Objects.equals(treeMap.get(key), i)) {
-                    System.out.println(capitalized(key)
-                            + " ".repeat(maxLengthWords - key.length())
-                            + " - " + i
-                            + " шт.");
-                }
-            }
-        }
-
-    }
 }
 
---
+++ Methods.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Methods {
+    public static ArrayList<String> maxLengthWord(ArrayList<String> picnic) {
+        picnic.sort(Comparator.comparingInt(String::length));
+        ArrayList<String> maxLengthSizeWords = new ArrayList<>();
+        int maxSizeWord = picnic.getLast().length();
+        for (int i = picnic.size() - 1; i > 0; i--) {
+            if (picnic.get(i).length() == maxSizeWord && !maxLengthSizeWords.contains(picnic.get(i).toLowerCase())) {
+                maxLengthSizeWords.add(picnic.get(i).toLowerCase());
+            } else break;
+        }
+        return maxLengthSizeWords;
+    }
+
+    public static HashMap<String, Integer> countProducts(ArrayList<String> picnic) {
+        HashMap<String, Integer> countProduct = new HashMap<>();
+        for (String product : picnic) {
+            if (!countProduct.containsKey(product.toLowerCase())) {
+                countProduct.put(product.toLowerCase(), 1);
+            } else {
+                int count = (Integer) countProduct.get(product.toLowerCase()) + 1;
+                countProduct.put(product.toLowerCase(), count);
+            }
+        }
+        return countProduct;
+    }
+
+    public static String capitalized(String string) {
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
+    }
+
+    public static void sortedTreeMapByValue(TreeMap<String, Integer> treeMap, int maxLengthWords) {
+        TreeSet<Integer> values = new TreeSet<>(Comparator.reverseOrder());
+        values.addAll(treeMap.values());
+        Set<String> keySet = treeMap.keySet();
+        for (Integer i : values) {
+            for (String key : keySet) {
+                if (Objects.equals(treeMap.get(key), i)) {
+                    System.out.println(capitalized(key)
+                            + " ".repeat(maxLengthWords - key.length())
+                            + " - " + i
+                            + " шт.");
+                }
+            }
+        }
+
+    }
+}
